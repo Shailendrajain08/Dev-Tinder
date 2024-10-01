35ded722cf5edfe2c208e449b124e9331df36e55
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-10-02 01:52:53 +0530

    working on routing

--- devTinder-backend/src/app.js
+++ devTinder-backend/src/app.js
@@ -4,16 +4,7 @@ const app = express();
 
 app.get("/user", (req, res) => {
   res.send({firstName: "Shailendra", lastName: "Jain"})
-})
-
-app.post("/user", (req, res) => {
-  res.send("Data successfully saved to database")
-})
-
-app.delete("/user", (req, res) => {
-  res.send("data deleted successfuly")
-})
-
+});
 
 app.listen(3000, () => {
   console.log("Server Started Successfully on", " http://localhost:3000/");
