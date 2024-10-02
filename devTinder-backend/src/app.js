66ec4021907f56071cf7f2d138fd640b4fb905e7
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-10-03 01:37:23 +0530

    Middlewares

--- devTinder-backend/src/app.js
+++ devTinder-backend/src/app.js
@@ -2,9 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({firstName: "Shailendra", lastName: "Jain"})
+const {adminAuth, userAuth} = require("./middlewares/auth")
+// Handle Auth Middleware for all GET POST, ... requests
+
+app.use("/admin", adminAuth);
+
+app.get ("/user", userAuth, (req, res) => {
+  res.send("User data")
+})
+
+app.get("/admin/getAllData", (req,res) => {
+  res.send("All Data send");
 });
+
+app.get("/admin/deleteUser", (req,res) => {
+  res.send("Deleted a user")
+})
 
 app.listen(3000, () => {
   console.log("Server Started Successfully on", " http://localhost:3000/");
