8281099b04b5cafceea55f5a49ee6e891f6f6c59
Shailendrajain08 <shailedra.jain0894.com>
2024-10-06 13:55:14 +0530

    created schema and send data to db

--- devTinder-backend/src/app.js
+++ devTinder-backend/src/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const connectDB = require ("./config/database")
-
 const app = express();
+const userModel = require('./models/user')
+
+app.post('/signup', async (req,res) => {
+  const userObj = {
+    firstName: "Shailendra",
+    lastName: "Jain",
+    emailId: "shailedra123@gmail.com",
+    password: "Shailendra123@"
+  }
+
+  const user = new userModel(userObj)
+  await user.save();
+  res.send("user added successfully")
+})
 
 connectDB()
 .then(() => {
