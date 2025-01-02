b7e2b653f27efe708575fae05c607e569c4f2dbc
Shailendrajain08 <shailendra.jain0894@gmail.com>
2025-01-03 00:29:23 +0530

    logout api completed

--- devTinder-backend/src/routes/auth.js
+++ devTinder-backend/src/routes/auth.js
@@ -73,4 +73,11 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.post('/logout', async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  })
+  res.status(200).send("Logout Successfully")
+})
+
 module.exports = router;
