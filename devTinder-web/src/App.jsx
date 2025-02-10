9157eac578172a69c687b3adf2aa2e32d8d17d61
Shailendrajain08 <shailendra.jain0894@gmail.com>
2025-02-11 00:37:41 +0530

    Routing is done basic component created

--- devTinder-web/src/App.jsx
+++ devTinder-web/src/App.jsx
@@ -1,9 +1,19 @@
-import NavBar from "./NavBar";
+import Body from "./Body";
+import Login from "./Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./Profile";
 
 function App() {
   return (
     <>
-    <NavBar/>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login/>} />
+            <Route path="/profile" element={<Profile/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
